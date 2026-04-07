78ea86a92808aa5bdecf4a1e5c8961579863b49e
Sean Alter <seanalter1@gmail.com>
2026-04-07 10:23:30 -0400

    ➕ Add sql commands to create DB

---
+++ backend/schema/001_create_users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Users (
+  user_id SERIAL PRIMARY KEY,
+  email text,
+  username text NOT NULL,
+  password_hash VARCHAR(72) NOT NULL,
+  date_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  last_transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  donation_percentage INT DEFAULT 10
+)
